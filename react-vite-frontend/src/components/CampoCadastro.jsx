d497403830277285ec27a5035cf3c033e06e9e74
flaviachristine <flavia.pereira@sptech.school>
2026-04-07 18:13:02 -0300

    Alterações nas imagens

--- react-vite-frontend/src/components/CampoCadastro.jsx
+++ react-vite-frontend/src/components/CampoCadastro.jsx
@@ -7,7 +7,7 @@ export function CampoCadastro(props) {
         <>
             <div className="p-[6%] w-1/2 h-1/1 rounded-3xl bg-[#FAF7FB] text-center" aria-label="Area com os campos de entrada do usuario para realizar o cadastro">
                 <MenuCadastroLogin tela={props.tela} setTela={props.setTela} />
-                <h2 className="mt-[8%] text-4xl font-semibold font-title">Cadastre-se</h2>
+                <h2 className="mt-[8%] text-4xl font-semibold font-title text-gray-900">Cadastre-se</h2>
                 <div className="flex flex-col space-y-3 items-center justify-center mt-8 mb-10">
                     <InputCadastro nome={"Nome"} placeholder={"Digite seu nome"} />
                     <InputCadastro nome={"Email"} placeholder={"Digite seu email"} />
--- react-vite-frontend/src/pages/Cadastro.jsx
+++ react-vite-frontend/src/pages/Cadastro.jsx
@@ -2,10 +2,14 @@ import { CampoCadastro } from "../components/CampoCadastro";
 
 export function Cadastro(props) {
     return <>
-        <div className="w-full h-screen flex items-center justify-center drop-shadow-2xl">
-            <div className="bg-gray-400 w-3/5 h-7/8 rounded-3xl" aria-label="Container principal para Cadastro">
-                <CampoCadastro tela = {props.tela} setTela = {props.setTela}/>
+        <div className="w-full h-screen flex items-center justify-center drop-shadow-2xl flex-wrap">
+            <div className="bg-[#EDE0F0] w-3/5 h-7/8 rounded-3xl flex" aria-label="Container principal para Cadastro">
+                <CampoCadastro tela={props.tela} setTela={props.setTela} />
+                <div className="w-1/2 flex justify-center items-center">
+                    <img src="/atelie-logo.png" alt="" className="w-2l" />
+                </div>
             </div>
+
         </div>
     </>
 }
