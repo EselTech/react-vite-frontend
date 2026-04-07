065376d5d0766be44cffd73f681347750b59ee28
flaviachristine <flavia.pereira@sptech.school>
2026-04-07 10:54:50 -0300

    Alterações nas fontes - Alterado de quicksand para nunito

--- react-vite-frontend/src/components/MenuCadastroLogin.jsx
+++ react-vite-frontend/src/components/MenuCadastroLogin.jsx
@@ -4,7 +4,7 @@ export function MenuCadastroLogin(props) {
     const estiloInativo = "w-1/2 rounded-3xl flex text-center items-center justify-center text-[#896D95] font-semibold"
 
     return <>
-        <div className="h-[8%] w-[62%] bg-white justify-self-center rounded-3xl flex shadow-sm font-quicksand">
+        <div className="h-[8%] w-[62%] bg-white justify-self-center rounded-3xl flex shadow-sm font-text">
             <div className={props.tela == "Cadastro" ? estiloInativo : estiloAtivo} onClick={() => {props.setTela("Login")}}>Entrar</div>
             <div className={props.tela == "Login" ? estiloInativo : estiloAtivo} onClick={() => {props.setTela("Cadastro")}}>Cadastrar</div>
         </div>
