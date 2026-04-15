42d18c744d25d1a11e936e4093e11833c086d1e6
luckas-lopes <luckas.silva@sptech.school>
2026-04-15 17:42:53 -0300

    Ajustando posição dos elementos no cadastro

--- react-vite-frontend/src/components/InputCadastro.jsx
+++ react-vite-frontend/src/components/InputCadastro.jsx
@@ -1,6 +1,6 @@
 export function InputCadastro(props){
     return <>
-        <label className="flex flex-col text-left font-text font-semibold">{props.nome}
+        <label className="flex flex-col text-left font-text font-semibold h-16">{props.nome}
             <input type="text" placeholder={props.placeholder} className="placeholder:text-gray-500 placeholder:font-medium placeholder:p-3.5 placeholder:text-sm  font-display bg-white border-[#EDE0F0] border rounded-md w-96 h-12 mt-1 text-xs font-semibold" />
         </label>
         
