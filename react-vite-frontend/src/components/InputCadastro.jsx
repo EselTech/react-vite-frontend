e323c5cab39020c42fdfd54e990a651390576ee3
luckas-lopes <luckas.silva@sptech.school>
2026-05-02 09:20:30 -0300

    Função de login atualizada

--- react-vite-frontend/src/components/InputCadastro.jsx
+++ react-vite-frontend/src/components/InputCadastro.jsx
@@ -1,8 +1,13 @@
-export function InputCadastro(props){
+export function InputCadastro(props) {
     return <>
-        <label className="flex flex-col text-left font-text font-semibold h-16">{props.nome}
-            <input type="text" placeholder={props.placeholder} className="placeholder:text-gray-500 placeholder:font-medium placeholder:p-3.5 placeholder:text-sm  font-display bg-white border-[#EDE0F0] border rounded-md w-96 h-12 mt-1 text-xs font-semibold" />
+        <label className="flex flex-col text-left font-text font-semibold h-16" >{props.nome}
+            <input
+                type="text"
+                placeholder={props.placeholder}
+                onChange={(e) => props.aoMudar(e.target.value)}
+                className="placeholder:text-gray-500 placeholder:font-medium placeholder:text-sm font-display bg-white border-[#EDE0F0] border rounded-md w-96 h-12 mt-1 text-xs font-semibold px-4 font-text"
+            />
         </label>
-        
+
     </>
 }
