ceebce88366d8432250b22a0d7ce28379eb4cf72
luckas-lopes <luckas.silva@sptech.school>
2026-05-08 11:30:42 -0300

    Toasts adicionados e lógica de login e cadastro aprimorada

--- react-vite-frontend/src/components/CampoCadastro.jsx
+++ react-vite-frontend/src/components/CampoCadastro.jsx
@@ -2,12 +2,15 @@ import { useState, useEffect } from "react";
 import { api } from "../provider/api";
 import { MenuCadastroLogin } from "./MenuCadastroLogin";
 import { InputCadastro } from "./InputCadastro";
+import toast, { Toaster } from "react-hot-toast";
 
 export function CampoCadastro(props) {
     const [nome, setNome] = useState("");
     const [email, setEmail] = useState("");
     const [username, setUsername] = useState("");
     const [senha, setSenha] = useState("");
+
+    const [inputsBloqueados, setInputsBloqueados] = useState(false)
 
     const [erros, setErros] = useState({});
 
@@ -35,7 +38,7 @@ export function CampoCadastro(props) {
         }
 
         setErros(novosErros);
-    }, [nome, email, username, senha]); 
+    }, [nome, email, username, senha]);
 
     async function cadastrar() {
         const temCamposVazios = !nome || !email || !username || !senha;
@@ -49,7 +52,11 @@ export function CampoCadastro(props) {
         try {
             const credenciais = { nome, email, username, senha, role: "USER" };
             const response = await api.post("/auth/registrar", credenciais);
-            console.log("Cadastro realizado:", response.data);
+            toast.success("Cadastro realizado com sucesso!")
+            setInputsBloqueados(true)
+            setTimeout(() => {
+                props.setTela("Login")
+            }, 2000)
         } catch (error) {
             console.error("Erro ao cadastrar:", error.response?.data || error.message);
         }
@@ -57,6 +64,7 @@ export function CampoCadastro(props) {
 
     return (
         <div className="p-[6%] w-1/2 h-1/1 rounded-l-3xl bg-[#FAF7FB] text-center">
+            <Toaster />
             <MenuCadastroLogin tela={props.tela} setTela={props.setTela} />
 
             <div className="flex flex-col space-y-3 items-center justify-center mt-8 mb-10">
@@ -65,18 +73,21 @@ export function CampoCadastro(props) {
                     placeholder={"Digite seu nome completo"}
                     aoMudar={setNome}
                     erro={erros.nome}
+                    inputsBloqueados={inputsBloqueados}
                 />
                 <InputCadastro
                     nome={"Email"}
                     placeholder={"Digite seu email"}
                     aoMudar={setEmail}
                     erro={erros.email}
+                    inputsBloqueados={inputsBloqueados}
                 />
                 <InputCadastro
                     nome={"Nome de Usuário"}
                     placeholder={"Digite seu nome de usuário"}
                     aoMudar={setUsername}
                     erro={erros.username}
+                    inputsBloqueados={inputsBloqueados}
                 />
                 <InputCadastro
                     nome={"Senha"}
@@ -84,6 +95,7 @@ export function CampoCadastro(props) {
                     placeholder={"Digite sua senha"}
                     aoMudar={setSenha}
                     erro={erros.senha}
+                    inputsBloqueados={inputsBloqueados}
                 />
             </div>
 
