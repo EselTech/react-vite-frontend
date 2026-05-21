4e1b0d49a80bf514a53f4507027405b211b5c25f
luckas-lopes <luckas.silva@sptech.school>
2026-05-21 10:24:30 -0300

    Toda integração da tela orçamentos concluída

--- react-vite-frontend/src/pages/Orcamentos.jsx
+++ react-vite-frontend/src/pages/Orcamentos.jsx
@@ -1,18 +1,19 @@
 import { useEffect, useState } from "react";
 import { CardOrcamento } from "../components/CardOrcamento";
 import { DrawerOrcamento } from "../components/DrawerOrcamento";
-import { DrawerDetalhes } from "../components/DrawerDetalhes";
+import { DrawerDetalhesOrcamento } from "../components/DrawerDetalhesOrcamento";
 import { Nav } from "../components/Nav";
+import { api } from "../provider/api";
 
 export function Orcamentos() {
     const [drawerIsOpen, setDrawerIsOpen] = useState(false);
     const [drawerDetalhesOpen, setDrawerDetalhesOpen] = useState(false);
     const [orcamentoSelecionado, setOrcamentoSelecionado] = useState(null);
-
     const [listaOrcamentos, setListaOrcamentos] = useState([]);
 
     const salvarNovoOrcamento = (novo) => {
-        setListaOrcamentos((prev) => [novo, ...prev]);
+        api.post("/orcamentos", novo)
+        carregarOrcamentos()
         setDrawerIsOpen(false);
     };
 
@@ -30,69 +31,79 @@ export function Orcamentos() {
         setOrcamentoSelecionado(orcamentoEditado);
     }
 
+    function carregarOrcamentos() {
+        api.get("/orcamentos").then(resposta => setListaOrcamentos(resposta.data))
+        console.log(listaOrcamentos);
+    }
+
+    useEffect(() => {
+        carregarOrcamentos()
+    }, [])
+
+
     return (
         <div className="flex">
-            <Nav tela="Orcamentos"/>
-        <div className="w-10/12 bg-white pl-20 pt-[4vh]">
-            <div className="w-11/12 mt-8 flex flex-wrap gap-2">
-                {listaOrcamentos.length > 0 ? (
-                    <>
-                        <div className="h-16 flex justify-between w-11/12 items-center mb-12">
-                            <div>
-                                <h1 className="text-[#695088] font-title font-semibold text-5xl self-start">
-                                    Meus Orçamentos
-                                </h1>
-                                <p className="text-gray-400 m-0">
-                                    Faça o orçamento de seus pedidos
-                                </p>
+            <Nav tela="Orcamentos" />
+            <div className="w-10/12 bg-white pl-20 pt-[4vh]">
+                <div className="w-11/12 mt-8 flex flex-wrap gap-2">
+                    {listaOrcamentos.length > 0 ? (
+                        <>
+                            <div className="h-16 flex justify-between w-11/12 items-center mb-12">
+                                <div>
+                                    <h1 className="text-[#695088] font-title font-semibold text-5xl self-start">
+                                        Meus Orçamentos
+                                    </h1>
+                                    <p className="text-gray-400 m-0">
+                                        Faça o orçamento de seus pedidos
+                                    </p>
 
+                                </div>
+                                <button
+                                    className="bg-linear-to-br from-[#896D95] to-[#C8A0C0] text-white rounded-full h-12 w-40 font-semibold cursor-pointer shadow-md transition-all duration-300 hover:hover:shadow-lg hover:brightness-110 active:scale-95"
+                                    onClick={() => setDrawerIsOpen(true)}
+                                >
+                                    + Novo Orçamento
+                                </button>
                             </div>
+
+                            {listaOrcamentos.map((orcamento, index) => (
+                                <CardOrcamento
+                                    key={index}
+                                    {...orcamento}
+                                    onClick={() => abrirDetalhes(orcamento)}
+                                />
+                            ))}
+                        </>
+                    ) : (
+                        <div className="w-2/4 text-center mx-auto mt-32 flex flex-col items-center">
+                            <div className="w-30 h-30 rounded-full flex justify-center items-center text-7xl bg-[#ede0f0] animate-bounce">
+                                🧾
+                            </div>
+                            <h1 className="font-title text-5xl font-bold text-[#695088]">Nenhum orçamento criado ainda</h1>
+                            <p className="text-gray-400 mt-4 font-text">Combine materiais e defina seus lucros de forma automatizada.</p>
                             <button
-                                className="bg-linear-to-br from-[#896D95] to-[#C8A0C0] text-white rounded-full h-12 w-40 font-semibold cursor-pointer shadow-md transition-all duration-300 hover:hover:shadow-lg hover:brightness-110 active:scale-95"
+                                className="bg-linear-to-br from-[#896D95] to-[#C8A0C0] text-white rounded-full h-12 w-56 font-semibold cursor-pointer mt-12"
                                 onClick={() => setDrawerIsOpen(true)}
                             >
-                                + Novo Orçamento
+                                Criar Primeiro Orçamento
                             </button>
                         </div>
+                    )}
+                </div>
 
-                        {listaOrcamentos.map((orc, index) => (
-                            <CardOrcamento
-                                key={index}
-                                {...orc}
-                                onClick={() => abrirDetalhes(orc)}
-                            />
-                        ))}
-                    </>
-                ) : (
-                    <div className="w-2/4 text-center mx-auto mt-32 flex flex-col items-center">
-                        <div className="w-30 h-30 rounded-full flex justify-center items-center text-7xl bg-[#ede0f0] animate-bounce">
-                            🧾
-                        </div>
-                        <h1 className="font-title text-5xl font-bold text-[#695088]">Nenhum orçamento criado ainda</h1>
-                        <p className="text-gray-400 mt-4 font-text">Combine materiais e defina seus lucros de forma automatizada.</p>
-                        <button
-                            className="bg-linear-to-br from-[#896D95] to-[#C8A0C0] text-white rounded-full h-12 w-56 font-semibold cursor-pointer mt-12"
-                            onClick={() => setDrawerIsOpen(true)}
-                        >
-                            Criar Primeiro Orçamento
-                        </button>
-                    </div>
-                )}
+                <DrawerOrcamento
+                    isOpen={drawerIsOpen}
+                    setDrawerIsOpen={setDrawerIsOpen}
+                    onSalvar={salvarNovoOrcamento}
+                />
+
+                <DrawerDetalhesOrcamento
+                    isOpen={drawerDetalhesOpen}
+                    setIsOpen={setDrawerDetalhesOpen}
+                    orcamento={orcamentoSelecionado}
+                    carregarOrcamentos={carregarOrcamentos}
+                />
             </div>
-
-            <DrawerOrcamento
-                isOpen={drawerIsOpen}
-                setDrawerIsOpen={setDrawerIsOpen}
-                onSalvar={salvarNovoOrcamento}
-            />
-
-            <DrawerDetalhes
-                isOpen={drawerDetalhesOpen}
-                setIsOpen={setDrawerDetalhesOpen}
-                orcamento={orcamentoSelecionado}
-                onAtualizar={atualizarOrcamento}
-            />
-        </div>
         </div>
     );
 }
